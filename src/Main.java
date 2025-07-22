affd78f7fe49b51852c6d511da07050822fbb19d
itamar <isantos.ads@gmail.com>
2025-07-21 22:08:56 -0300

    initial comit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+package src;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/api/hello", new HelloHandler());
+        server.setExecutor(null);
+        System.out.println("API rodando em http://localhost:8080/api/hello");
+        server.start();
+    }
+
+    static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "{\"message\": \"Olá, DevSecOps!\"}";
+            t.getResponseHeaders().set("Content-Type", "application/json; charset=UTF-8");
+            t.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+} 
